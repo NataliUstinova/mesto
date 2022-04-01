a6318ad4c04a4e610c8232557a91c5d63400296a
Natalia Ustinova <nataliaustinova58@gmail.com>
2022-04-01 23:05:15 +0300

    feat: open-close popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,14 @@
-// Находим форму в DOM
-let formElement = document.querySelector(); // Воспользуйтесь методом querySelector()
-// Находим поля формы в DOM
-let nameInput = // Воспользуйтесь инструментом .querySelector()
-let jobInput = // Воспользуйтесь инструментом .querySelector()
+const profile = document.querySelector('.profile');
+const profileEditButton = profile.querySelector('.profile__edit-button');
+const popupCloseButton = document.querySelector('.popup__close');
+const popup = document.querySelector('.popup');
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
-
-    // Получите значение полей jobInput и nameInput из свойства value
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-
-    // Вставьте новые значения с помощью textContent
+profileEditButton.addEventListener("click", openPopup);
+function openPopup() {
+  popup.classList.add("popup_opened");
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler); 
+popupCloseButton.addEventListener("click", closePopup);
+function closePopup() {
+  popup.classList.remove("popup_opened");
+}
