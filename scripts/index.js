ddd7d1e7dd84fca5d01a237b04242bcf13c0ab69
Natalia Ustinova <nataliaustinova58@gmail.com>
2022-04-02 00:34:05 +0300

    feat: Profile Edit

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,31 @@
 const profile = document.querySelector('.profile');
 const profileEditButton = profile.querySelector('.profile__edit-button');
-const popupCloseButton = document.querySelector('.popup__close');
 const popup = document.querySelector('.popup');
+const popupCloseButton = popup.querySelector('.popup__close');
+const popupSaveButton = popup.querySelector('.popup__save');
 
-profileEditButton.addEventListener("click", openPopup);
+let profileName = profile.querySelector('.profile__name');
+let profileJob = profile.querySelector('.profile__job');
+let formElement = popup.querySelector('.editProfile');
+let nameInput = formElement.querySelector('.popup__input_value_name');
+let jobInput = formElement.querySelector('.popup__input_value_job');
+
+
+profileEditButton.addEventListener('click', openPopup);
 function openPopup() {
-  popup.classList.add("popup_opened");
+  popup.classList.add('popup_opened');
 }
 
-popupCloseButton.addEventListener("click", closePopup);
+popupCloseButton.addEventListener('click', closePopup);
 function closePopup() {
-  popup.classList.remove("popup_opened");
+  popup.classList.remove('popup_opened');
 }
+
+popupSaveButton.addEventListener('click', formSubmitHandler); 
+function formSubmitHandler (evt) {
+    evt.preventDefault(); 
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+    closePopup();
+}
+
