9f294dfe36f2cc2e9cd97a89209dd7daa07c9b19
Natalia Ustinova <nataliaustinova58@gmail.com>
2022-06-02 13:27:05 +0300

    feat: User info class

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,5 +1,17 @@
 export default class UserInfo {
-  constructor({}) {
-    
+  constructor({nameElementSelector, userInfoElementSelector}) {
+    this._nameElement = document.querySelector(nameElementSelector);
+    this._userInfoElement = document.querySelector(userInfoElementSelector);
+  }
+
+  getUserInfo() {
+    return {
+      name: this._nameElement.textContent,
+      job: this._userInfoElement.textContent,
+    }
+  }
+  setUserInfo(name, job) {
+    this._nameElement.textContent = name.value;
+    this._userInfoElement.textContent = job.value;
   }
 }
