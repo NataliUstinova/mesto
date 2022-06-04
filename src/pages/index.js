6d51141005affa36ba0d0e46875389b2b5f0cf86
Natalia Ustinova <nataliaustinova58@gmail.com>
2022-06-04 12:05:06 +0300

    fix: constants refactor

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,51 +1,31 @@
 import './index.css';
-import { initialCards } from '../components/constants.js';
+import { initialCards,
+  popupEditProfileSelector,
+  profileEditButton, 
+  formProfile,
+  nameInput,
+  jobInput,
+  popupPicAddSelector,
+  formCard,
+  newPicButton,
+  validationOptions,
+  cardsListSelector,
+  cardTemplate,
+  showImagePopupSelector,
+  
+  } from '../components/constants.js';
 import Section from '../components/Section.js'
 import Card from '../components/Card.js'
 import FormValidator from '../components/FormValidator.js';
-// import Popup from '../components/Popup.js';
 import PopupWithImage from '../components/PopupWithImage.js';
 import PopupWithForm from '../components/PopupWithForm.js';
 import UserInfo from '../components/UserInfo.js';
-
-
-const popupEditProfile = document.querySelector('.popup_profile-info');
-const popupEditProfileSelector = '.popup_profile-info';
-const profile = document.querySelector('.profile'); // профиль
-const profileEditButton = profile.querySelector('.profile__edit-button'); //кнопка редактирования
-//
-// //form popup edit profile
-const formProfile = popupEditProfile.querySelector('.popup__form'); // форма
-const nameInput = formProfile.querySelector('.popup__input_value_name'); //поле ввода имени
-const jobInput = formProfile.querySelector('.popup__input_value_job'); //поле ввода деятельности
-//
-// //popup add pic
-const popupPicAddSelector = '.popup_add-pic';
-// //popup add pic form
-const formCard = document.querySelector('.popup__form_add-pic');
-// //popup add pic buttons
-const newPicButton = profile.querySelector('.profile__add-button');
-
-//validation options
-const validationOptions = {
-  formElementSelector: '.popup__form',
-  inputElementSelector: '.popup__input',
-  submitButtonSelector: '.popup__save',
-  inactiveButtonClass: 'popup__save_disabled',
-  inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__error_visible',
-}
 
 const editProfileValidation = new FormValidator(validationOptions, formProfile);
 const addCardValidation = new FormValidator(validationOptions, formCard);
 
 editProfileValidation.enableValidation();
 addCardValidation.enableValidation();
-
-//Card section
-const cardsListSelector = '.cards__list';
-const cardTemplate = '.card__template';
-
 
 function createCard(item) {
   return new Card(item, cardTemplate, () => {
@@ -73,7 +53,6 @@ const profilePopup = new PopupWithForm({
     editProfileValidation.hideErrors();
     profilePopup.resetForm();
     editProfileValidation.toggleButtonState();
-    
   }
   }, popupEditProfileSelector);
 profilePopup.setEventListeners();
@@ -91,7 +70,6 @@ const addCardPopup = new PopupWithForm({
       name: item.name,
       link: item.link
     };
-    
     cardsList.addItem(createCard(newCardInput));
     addCardPopup.close();
     addCardPopup.resetForm();
@@ -107,6 +85,5 @@ addCardPopup.setEventListeners()
 
 newPicButton.addEventListener('click', () => addCardPopup.open());
 
-const showImagePopupSelector = '.popup_show-pic';
 const showImagePopup = new PopupWithImage(showImagePopupSelector);
 showImagePopup.setEventListeners();
