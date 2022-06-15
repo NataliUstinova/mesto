47d5d643e1c4107a0bb11a3809b8d96b17d81ae7
Natalia Ustinova <nataliaustinova58@gmail.com>
2022-06-15 22:03:52 +0300

    feat: class Api - getUserInfoServer() #1

---
+++ src/components/Api.js
@@ -0,0 +1,24 @@
+class Api {
+  constructor({baseUrl, headers}) {
+    this._baseUrl = baseUrl;
+    this._headers = headers;
+  }
+  
+  
+  getUserInfoServer() {
+    return  fetch(`${this._baseUrl}/users/me`, {
+      headers: this._headers
+    })
+      .then(res => res.ok ? res.json() : Promise.reject(res.status))
+  }
+  getInitialCards() {
+    // ...
+  }
+}
+const api = new Api({
+  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-43',
+  headers: {
+    authorization: '46f10111-7535-41c9-bac2-e4a6e0f14e0d',
+    'Content-Type': 'application/json'
+  }
+}); 
